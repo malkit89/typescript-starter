c3e2d047a9a5c79b7a6b85a7a9891a45f15913b2
Malkit <kleric89@gmail.com>
2021-02-21 14:16:59 +0000

    Creato modulo cats

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,14 +3,14 @@ import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
-    private readonly cats: Cat[] = [];
+    private readonly cats: Cat[] = FakeData();
 
     create(cat: Cat) {
         this.cats.push(cat);
     }
 
     findAll(): Promise<Cat[]> {
-        let self=this;
+        let self = this;
         return new Promise(function (resolve) {
             setTimeout(() => {
                 resolve(self.cats);
@@ -18,4 +18,17 @@ export class CatsService {
         })
 
     }
+}
+
+function FakeData(): Cat[] {
+    let result: Cat[] = [];
+    for (let i = 0; i < 5; i++) {
+        let c = {
+            name: "Nome " + i,
+            age: i+20,
+            breed: "Item " + i
+        };
+        result.push(c);
+    }
+    return result;
 }
